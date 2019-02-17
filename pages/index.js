88446b235cfb4617a7d4458c98706592700516b7
jeancarlo_flores93@hotmail.com <jeancarlo_flores93@hotmail.com>
2019-02-16 22:56:06 -0500

    First Render

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+export default class extends Component {
+    render() {
+        return <div>
+            <h1>Hola Mundo!</h1>
+            <p>Bienvenidos al curso de Next.js</p>
+        </div>
+    }
+}
