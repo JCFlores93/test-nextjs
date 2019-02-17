272e31bf2ecb1daf7451900a594689eaf4bc069a
jeancarlo_flores93@hotmail.com <jeancarlo_flores93@hotmail.com>
2019-02-17 00:51:52 -0500

    custom errors

--- pages/channel.js
+++ pages/channel.js
@@ -2,49 +2,61 @@ import 'isomorphic-fetch'
 import Layout from '../components/Layout'
 import ChannelGrid from '../components/ChannelGrid'
 import PodcastList from '../components/PodcastList'
+import Error from './error'
 
 export default class extends React.Component {
 
-  static async getInitialProps({ query }) {
-    const idChannel = query.id
+    static async getInitialProps({ query, res }) {
+        const idChannel = query.id
+        try {
+            const [reqChannel, reqSeries, reqAudios] = await Promise.all([
+                fetch(`https://api.audioboom.com/channels/${idChannel}`),
+                fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+                fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+            ])
 
-    const [reqChannel, reqSeries, reqAudios] = await Promise.all([
-      fetch(`https://api.audioboom.com/channels/${idChannel}`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
-    ])
+            if (reqChannel.status >= 404) {
+                res.statusCode = reqChannel.status
+                return { channel: null, audioClips: null, series: null, statusCode: 404 }
+            }
+            const dataChannel = await reqChannel.json()
+            const channel = dataChannel.body.channel
 
-    const dataChannel = await reqChannel.json()
-    const channel = dataChannel.body.channel
+            const dataAudios = await reqAudios.json()
+            const audioClips = dataAudios.body.audio_clips
 
-    const dataAudios = await reqAudios.json()
-    const audioClips = dataAudios.body.audio_clips
+            const dataSeries = await reqSeries.json()
+            const series = dataSeries.body.channels
 
-    const dataSeries = await reqSeries.json()
-    const series = dataSeries.body.channels
+            return { channel, audioClips, series, statusCode: 200 }
+        } catch (error) {
+            return { channel: null, audioClips: null, series: null, statusCode: 503 }
+        }
 
-    return { channel, audioClips, series }
-  }
+    }
 
-  render() {
-    const { channel, audioClips, series } = this.props
+    render() {
+        const { channel, audioClips, series } = this.props
+        if (statusCode !== 200) {
+            // <div>Se rompio el server</div>
+            return <Error {...statusCode} />
+        }
+        return <Layout title={channel.title}>
+            <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
 
-    return <Layout title={channel.title}>
-      <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
-      
-      <h1>{ channel.title }</h1>
+            <h1>{channel.title}</h1>
 
-      { series.length > 0 &&
-        <div>
-          <h2>Series</h2>
-          <ChannelGrid channels={ series } />
-        </div>
-      }
+            {series.length > 0 &&
+                <div>
+                    <h2>Series</h2>
+                    <ChannelGrid channels={series} />
+                </div>
+            }
 
-      <h2>Ultimos Podcasts</h2>
-      <PodcastList podcasts={ audioClips } />
+            <h2>Ultimos Podcasts</h2>
+            <PodcastList podcasts={audioClips} />
 
-      <style jsx>{`
+            <style jsx>{`
         .banner {
           width: 100%;
           padding-bottom: 25%;
@@ -63,6 +75,6 @@ export default class extends React.Component {
           margin: 0;
         }
       `}</style>
-    </Layout>
-  }
+        </Layout>
+    }
 }
