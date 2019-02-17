6f50638f85fa562c37dd4ba650de63519a1eb936
jeancarlo_flores93@hotmail.com <jeancarlo_flores93@hotmail.com>
2019-02-16 23:04:42 -0500

    estilos globales

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,26 @@ export default class extends Component {
         return <div>
             <h1>Hola Mundo!</h1>
             <p>Bienvenidos al curso de Next.js</p>
+            <img src='/static/platzi-logo.png' alt='Platzi'></img>
+            <style jsx>{`
+            h1 {
+                color:red
+            }
+            :global(div p){
+                color:green
+            }
+            img {
+                max-width: 50%;
+                display:block;
+                margin: 0 auto
+            }
+            `}</style>
+
+            <style jsx global>{`
+            body {
+                background: white
+            }
+            `}</style>
         </div>
     }
 }
